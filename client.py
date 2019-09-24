d5d961d9b78f877cde5ad3be0a44dbea198eb69d
williamgdo <williamgdo.app@gmail.com>
2019-09-24 18:21:59 -0300

    server identifies the option (send or receive file)

--- client.py
+++ client.py
@@ -11,13 +11,13 @@ print('Se conectando a ' + host + ', porta ' + str(port))
 sock.connect((host, port))  # conecta o socket na porta em que o server esta escutando
 
 try:
-    opcao = int(input("Digite 1 para enviar um arquivo ou 2 para baixar o ultimo enviado: "))
+    opcao = int(input("Digite 1 para enviar ou 2 para receber um arquivo do servidor: "))
     if opcao == 1:       # enviar
-        # sock.sendall(bytes(1))      # avisa o servidor que o cliente vai ENVIAR um arquivo
+        sock.sendall(bytes([1]))      # avisa o servidor que o cliente vai ENVIAR um arquivo
 
         # recebe o nome do arquivo e carrega os bytes para "file_data"
         try:
-            file_name = str(input("Digite o nome do arquivo: "))
+            file_name = str(input("Digite endereco do arquivo: "))
             file = open(file_name, "rb")
             file_data = file.read()
             file.close()
@@ -26,7 +26,15 @@ try:
         except Exception as ex:
             print(ex)
     elif opcao == 2:     # receber
-        # sock.sendall(bytes(2))      # avisa o servidor que o cliente vai RECEBER um arquivo
+        sock.sendall(bytes([2]))      # avisa o servidor que o cliente vai RECEBER um arquivo
+        
+        try:
+            file_name = str(input("Digite nome do arquivo a ser recebido: "))
+            sock.sendall(bytes(file_name, encoding = 'utf8'))      # envia todos os bytes de "file_data" para o socket
+        except Exception as ex:
+            print(ex)
+
+        file_data = bytes(0)
         
         # recebe de 0 a 4096 enquanto o arquivo nao for vazio 
         while True:
@@ -41,7 +49,6 @@ try:
          # se o arquivo nao for vazio, salva com o nome server_file
         if len(file_data) > 0:     
             print('Salvando dados...')
-            file_name = "server_file"
             write_file = open(file_name, 'wb')
             write_file.write(file_data)
             write_file.close() 
--- server.py
+++ server.py
@@ -21,31 +21,39 @@ while True:
         print('Conexao de ' + str(client_address))
         file_data = bytes(0)
 
-        # data = connection.recv(1)   # recebe "opcao" do cliente (enviar ou receber arquivo)
+        data = connection.recv(1)   # recebe "opcao" do cliente (enviar ou receber arquivo)
+        
 
-        # if int(data) == 1:      # cliente envia um arquivo
+        if int.from_bytes(data, byteorder='little') == 1:      # cliente envia um arquivo
             # recebe de 0 a 4096 enquanto o arquivo nao for vazio 
             
-        while True: 
-            data = connection.recv(4096)    
-            if data:
-                file_data += data
-                print('Recebido ' + str(len(data)) +' bytes')
-            else:
-                print('Não há mais dados de ' + str(client_address) + '. Total de bytes recebidos: ' + str(len(file_data)))
-                break    
-        # se o arquivo nao for vazio, salva com o nome {ip do cliente, socket#}_file
-        if len(file_data) > 0:   
-            print('Salvando dados...')
-            file_name = str(client_address) + "_file"
-            write_file = open(file_name, 'wb')
-            write_file.write(file_data)
-            write_file.close() 
+            while True: 
+                data = connection.recv(4096)    
+                if data:
+                    file_data += data
+                    print('Recebido ' + str(len(data)) +' bytes')
+                else:
+                    print('Não há mais dados de ' + str(client_address) + '. Total de bytes recebidos: ' + str(len(file_data)))
+                    break    
+            # se o arquivo nao for vazio, salva com o nome {ip do cliente, socket#}_file
+            if len(file_data) > 0:   
+                print('Salvando dados...')
+                file_name = str(client_address) + "_file"
+                write_file = open("server_files/" + file_name, 'wb')
+                write_file.write(file_data)
+                write_file.close() 
 
-        # elif int(data) == 2:    # cliente recebe um arquivo
-        #     connection.sendall(data)
-        # else:
-        #     print('Opcao invalida.')
+        elif int.from_bytes(data, byteorder='little') == 2:    # cliente recebe um arquivo
+            file_name = connection.recv(4096)   # recebe o nome do arquivo a ser baixado 
+
+            file_name = str(file_name.decode("utf-8"))
+            file = open("server_files/" + file_name, "rb")
+            file_data = file.read()
+            file.close()
+            print('Enviando arquivo "' + file_name + '" para ' + str(client_address) + '...')
+            connection.sendall(file_data)
+        else:
+            print('Opcao invalida.')
 
     finally:
         connection.close() # finaliza a conecxao
